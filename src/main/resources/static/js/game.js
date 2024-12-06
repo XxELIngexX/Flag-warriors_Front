37f3854ffe73c48e23c0319aabca3aa6fcaa2413
ASUSCESAR\cesar <cesar.amaya.gomes@gmail.com>
2024-12-06 18:26:35 -0500

    no message

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,12 +1,10 @@
 let avatar;
-
 const gameScene = new Game();
-
 const config = {
     type: Phaser.AUTO,
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,  
+        autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1440,
         height: 1080,
     },
@@ -20,16 +18,7 @@ const config = {
     scene: [gameScene]
 };
 
+let game; // Variable global para mantener referencia
 gameScene.init().then(() => {
-    new Phaser.Game(config);
+    game = new Phaser.Game(config);
 });
-
-const EquipoA = 1;
-const EquipoB = 2;
-
-function actualizarPuntuaciones() {
-    const puntuacionA = apiclient.getTeamById().getScore();
-    const puntuacionB = apiclient.getTeamById().getScore();
-    document.getElementById('equipoA').textContent = `Equipo Azul: ${puntuacionA}`;
-    document.getElementById('equipoB').textContent = `Equipo Naranja: ${puntuacionB}`;
-}

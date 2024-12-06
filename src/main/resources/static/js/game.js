9d01e7b6733eb2a3524952a3dee4941507426cf1
ASUSCESAR\cesar <cesar.amaya.gomes@gmail.com>
2024-12-06 15:34:12 -0500

    no message

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,45 +1,35 @@
-
 let avatar;
 
-    const gameScene = new Game();
+const gameScene = new Game();
 
-    let config = {
-        type: Phaser.AUTO,
-        scale: {
-            mode: Phaser.Scale.FIT, // escala automáticamente
-            autoCenter: Phaser.Scale.CENTER_BOTH, // centra automáticamente
-            width: 1440, 
-            height: 1080, 
-        },
-        physics: {
-            default: "arcade", // tipo de física
-            arcade: {
-                gravity: { y: 0 }, // la gravedad
-                debug: false // para depurar
-            }
-        },
-        scene: [gameScene]
-        
-    }
-    gameScene.init().then(() => {
-        let juego = new Phaser.Game(config);
-    });
-    
+const config = {
+    type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,  
+        width: 1440,
+        height: 1080,
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: [gameScene]
+};
 
+gameScene.init().then(() => {
+    new Phaser.Game(config);
+});
 
+const EquipoA = 1;
+const EquipoB = 2;
 
-
-
-const EquipoA = 1; // Define el nombre del equipo azul
-const EquipoB = 2; // Define el nombre del equipo naranja
-
-// Llama a esta función cuando necesites actualizar las puntuaciones
 function actualizarPuntuaciones() {
-    // Obtén las puntuaciones de los equipos usando el método de apiclient
     const puntuacionA = apiclient.getTeamById().getScore();
     const puntuacionB = apiclient.getTeamById().getScore();
-
-    // Actualiza el contenido de los párrafos
     document.getElementById('equipoA').textContent = `Equipo Azul: ${puntuacionA}`;
     document.getElementById('equipoB').textContent = `Equipo Naranja: ${puntuacionB}`;
 }
